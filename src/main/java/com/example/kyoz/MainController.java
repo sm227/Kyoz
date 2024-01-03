ba04d86bf41ed3404124b4f3cb19821324989e83
Seungmin Ham <hamsm3184@naver.com>
2024-01-03 14:25:52 +0900

    update

--- src/main/java/com/example/kyoz/MainController.java
+++ src/main/java/com/example/kyoz/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
 
-    @GetMapping("/sbb")
+    @GetMapping("sbb")
     @ResponseBody
     public String index() {
         return "안녕하세요 sbb에 오신것을 환영합니다.";
